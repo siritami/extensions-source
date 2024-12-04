b2fd00ca59734150e89f8b4d456b6a3b142e9db3
siritami <103488094+siritami@users.noreply.github.com>
2024-12-04 11:25:01 +0700

    Update DocTruyen3Q.kt

--- src/vi/doctruyen3q/src/eu/kanade/tachiyomi/extension/vi/doctruyen3q/DocTruyen3Q.kt
+++ src/vi/doctruyen3q/src/eu/kanade/tachiyomi/extension/vi/doctruyen3q/DocTruyen3Q.kt
@@ -38,11 +38,11 @@ class DocTruyen3Q : WPComics(
     override fun popularMangaSelector() = "div.item-manga div.item"
 
     override fun popularMangaFromElement(element: Element) = SManga.create().apply {
-        element.select("h3 a").let {
+        element.selectFirst("h3 a")?.let {
             title = it.text()
             setUrlWithoutDomain(it.attr("abs:href"))
         }
-        thumbnail_url = element.selectFirst("img")?.attr("abs:src")
+        thumbnail_url = document.selectFirst("img.image-item")?.attr("abs:src")
     }
 
     override fun searchMangaSelector() = popularMangaSelector()
