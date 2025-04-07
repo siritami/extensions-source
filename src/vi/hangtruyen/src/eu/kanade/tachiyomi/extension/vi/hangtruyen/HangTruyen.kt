fff504f40a29f9f13fffa7dd96f93151589966a4
siritami <103488094+siritami@users.noreply.github.com>
2025-04-07 12:36:45 +0700

    Update HangTruyen.kt

--- src/vi/hangtruyen/src/eu/kanade/tachiyomi/extension/vi/hangtruyen/HangTruyen.kt
+++ src/vi/hangtruyen/src/eu/kanade/tachiyomi/extension/vi/hangtruyen/HangTruyen.kt
@@ -27,6 +27,8 @@ class HangTruyen :
     ),
     ConfigurableSource {
 
+    private val chapterDateFormat = SimpleDateFormat("MM/dd/yyyy", Locale.ROOT)
+
     override val useLoadMoreRequest = LoadMoreStrategy.Never
     override val useNewChapterEndpoint = false
 
