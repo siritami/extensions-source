dc7a36717b67bdc5307bd917d014a59e6bac4c27
siritami <103488094+siritami@users.noreply.github.com>
2025-04-12 22:05:08 +0700

    1

--- src/vi/hentaicube/src/eu/kanade/tachiyomi/extension/vi/hentaicube/HentaiCB.kt
+++ src/vi/hentaicube/src/eu/kanade/tachiyomi/extension/vi/hentaicube/HentaiCB.kt
@@ -25,7 +25,7 @@ import java.util.Locale
 class HentaiCB :
     Madara(
         "CBHentai",
-        "https://hentaicb.fit",
+        "https://hentaicb.bar",
         "vi",
         SimpleDateFormat("dd/MM/yyyy", Locale("vi")),
     ),
