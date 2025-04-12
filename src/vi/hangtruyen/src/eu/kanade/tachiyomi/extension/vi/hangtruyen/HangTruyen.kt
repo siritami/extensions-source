3ce555c303935e3ca236dc3228721f70c766c5e3
siritami <103488094+siritami@users.noreply.github.com>
2025-04-12 19:28:09 +0700

    Update HangTruyen.kt

--- src/vi/hangtruyen/src/eu/kanade/tachiyomi/extension/vi/hangtruyen/HangTruyen.kt
+++ src/vi/hangtruyen/src/eu/kanade/tachiyomi/extension/vi/hangtruyen/HangTruyen.kt
@@ -75,22 +75,8 @@ class HangTruyen : ParsedHttpSource() {
 
     private val queryParam = "keyword"
 
-    override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
+    override fun searchMangaRequest(page: Int, query: String): Request {
         val url = "$baseUrl/$searchPath".toHttpUrl().newBuilder()
-
-        filters.forEach { filter ->
-            when (filter) {
-                is GenreFilter -> filter.toUriPart()?.let { url.addPathSegment(it) }
-                is StatusFilter -> filter.toUriPart()?.let { url.addQueryParameter("status", it) }
-                else -> {}
-            }
-        }
-
-        url.apply {
-            addQueryParameter(queryParam, query)
-            addQueryParameter("page", page.toString())
-            addQueryParameter("sort", "0")
-        }
 
         return GET(url.toString(), headers)
     }
