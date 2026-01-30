4a23e0d16498475c87e7a7c8230a7e272bb18698
siritami <103488094+siritami@users.noreply.github.com>
2026-01-30 11:05:37 +0700

    Update MiMi.kt

--- src/vi/mimi/src/eu/kanade/tachiyomi/extension/vi/mimi/MiMi.kt
+++ src/vi/mimi/src/eu/kanade/tachiyomi/extension/vi/mimi/MiMi.kt
@@ -3,12 +3,10 @@ package eu.kanade.tachiyomi.extension.vi.mimi
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.network.interceptor.rateLimit
 import eu.kanade.tachiyomi.source.model.FilterList
-import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
-import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Interceptor
 import okhttp3.Request
