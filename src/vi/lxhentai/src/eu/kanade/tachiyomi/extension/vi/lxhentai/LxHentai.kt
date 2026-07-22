4093190a798b8c74a52fc5137d329c003507dade
fiorenmas <103488094+siritami@users.noreply.github.com>
2026-07-22 14:53:09 +0700

    Update LxHentai.kt

--- src/vi/lxhentai/src/eu/kanade/tachiyomi/extension/vi/lxhentai/LxHentai.kt
+++ src/vi/lxhentai/src/eu/kanade/tachiyomi/extension/vi/lxhentai/LxHentai.kt
@@ -1,5 +1,6 @@
 package eu.kanade.tachiyomi.extension.vi.lxhentai
 
+import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
