7cea9cc57b573f620e5c60fef5888350cd66f29e
fiorenmas <103488094+siritami@users.noreply.github.com>
2026-07-24 06:30:54 +0700

    1

--- src/vi/otakusic/src/eu/kanade/tachiyomi/extension/vi/otakusic/Dto.kt
+++ src/vi/otakusic/src/eu/kanade/tachiyomi/extension/vi/otakusic/Dto.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.json.JsonPrimitive
 
 @Serializable
 class ChaptersResponse(
-    val data: List<ChapterDto> = emptyList(),
+    val data: List<ChapterDto>,
 )
 
 @Serializable
@@ -14,8 +14,6 @@ class ChapterDto(
     @SerialName("chapter_name") val chapterName: JsonPrimitive,
     @SerialName("chapter_slug") val chapterSlug: String,
     @SerialName("chapter_original_slug") val chapterOriginalSlug: String,
-    @SerialName("manga_slug") val mangaSlug: String,
-    @SerialName("is_locked") val isLocked: Boolean = false,
     @SerialName("api_url") val apiUrl: String? = null,
     @SerialName("updated_at") val updatedAt: String? = null,
     @SerialName("public_at") val publicAt: String? = null,
