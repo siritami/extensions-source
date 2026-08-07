6255d4db36bd74bcaf5a6d1e0b7697b08470ba7a
fiorenmas <103488094+siritami@users.noreply.github.com>
2026-08-07 17:44:10 +0700

    Update KhoManhwa.kt

--- src/vi/khomanhwa/src/eu/kanade/tachiyomi/extension/vi/khomanhwa/KhoManhwa.kt
+++ src/vi/khomanhwa/src/eu/kanade/tachiyomi/extension/vi/khomanhwa/KhoManhwa.kt
@@ -197,7 +197,6 @@ abstract class KhoManhwa : KeiSource() {
     }
 
     // ============================== Related ===============================
-
     override val supportsRelatedMangas: Boolean = true
 
     override suspend fun fetchRelatedMangaList(manga: SManga): List<SManga> {
