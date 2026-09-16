c2035c0e69dadf9f54d02a2e8b4e405d10d161fa
siritami <103488094+siritami@users.noreply.github.com>
2026-09-17 03:05:57 +0700

    Update imgx-v4-reader.js

--- src/vi/moetruyen/assets/imgx-v4-reader.js
+++ src/vi/moetruyen/assets/imgx-v4-reader.js
@@ -38,8 +38,8 @@
             hexPreview(bytes.slice(12), 4) !== "56503858") {
             return "unknown";
         }
-        const width = 1 + bytes[20] + (bytes[21] << 8) + (bytes[22] << 16);
-        const height = 1 + bytes[23] + (bytes[24] << 8) + (bytes[25] << 16);
+        const width = 1 + bytes[22] + (bytes[23] << 8) + (bytes[24] << 16);
+        const height = 1 + bytes[25] + (bytes[26] << 8) + (bytes[27] << 16);
         return `${width}x${height}`;
     };
     const imgxDimensions = (bytes) => bytes.byteLength >= 13
